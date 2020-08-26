3d930c6c395ca71076cab2be032dccef8760ef65
David Elisma <david.elisma@tbs-sct.gc.ca>
2020-08-26 10:42:43 -0400

    fix: Update createIngress.sh

--- ingress/createIngress.sh
+++ ingress/createIngress.sh
@@ -2,5 +2,5 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.26.2/deploy/static/mandatory.yaml
 kubectl config set-context --current --namespace ingress-nginx
 kubectl apply -f ingress-config.yaml
-kubectl apply -f $1
+kubectl apply -f ingress-generic.yaml
 kubectl delete LimitRange/ingress-nginx
