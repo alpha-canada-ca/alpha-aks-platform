5b686926b6bac9f4445e374bb12ad69761a73fd4
Ryan Hyma <ryan.hyma@gmail.com>
2020-09-17 13:50:43 -0400

    Moving to drupal repo

--- drupal/deleteNFSShare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export AZURE_STORAGE_CONNECTION_STRING=`az storage account show-connection-string --name tbsnfs --resource-group tbs-nfs-rg -o tsv`
-echo ConnectionString:$AZURE_STORAGE_CONNECTION_STRING
-az storage share delete \
-  --name $1 \
-  --connection-string $AZURE_STORAGE_CONNECTION_STRING \
-  --verbose 
